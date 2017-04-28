bd8eaaf70ffb6a86560ecce360d3f5433df7b23b
Angelo Veltens <angelo.veltens@codecentric.de>
2017-04-28 16:33:00 +0200

    build and test custom image

--- tests/image_test.py
+++ tests/image_test.py
@@ -2,9 +2,8 @@ import pytest
 import docker
 
 @pytest.fixture(scope="module", autouse=True)
-def container():
-    client = docker.from_env()
-    container = client.containers.run('jenkins', name="jenkins_test_container", detach=True)
+def container(client, image):
+    container = client.containers.run(image.id, name="jenkins_test_container", detach=True)
     yield container
     container.remove(force=True)
 
